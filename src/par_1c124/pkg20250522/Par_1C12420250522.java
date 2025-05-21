4a0c1486f3f8cd43fc82650eeb3503108693f654
LukaBevilacqua <lukabevilacqua11@gmail.com>
2025-05-21 18:54:36 -0300

    a

---
+++ src/par_1c124/pkg20250522/Par_1C12420250522.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package par_1c124.pkg20250522;
+
+/**
+ *
+ * @author lukab
+ */
+public class Par_1C12420250522 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
